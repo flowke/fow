b6a26db99d8d4b40bd582c4cb349b97af8bc0688
春田石雨 <flowke@163.com>
2019-09-29 22:35:36 +0800

    toArr

--- packages/visitor/src/toArr.js
+++ packages/visitor/src/toArr.js
@@ -1,39 +1,86 @@
 // resolve object's values, keys
-export default function toArr(obj) {
-  return new Value().generate(obj)
+export default function toArr(obj, generate=true) {
+  let v = new Value(obj)
+  if (generate) v.generate()
+  return v
 }
 
 class Value {
-  constructor(){
-
+  constructor(obj={}){
+    this.__rowValue = obj;
+    this.__hasGenerate = false;
     this.keys = [];
     this.values = [];
     this.entries = [];
   }
 
-  generate(obj={}){
-    for (const key in obj) {
-      const elt = obj[key];
-      this.keys.push(key);
-      this.values.push(elt);
-      this.arr.push([key, elt]);
-    }
+  get(){
+    if (!this.__hasGenerate) this.generate();
     return this;
   }
 
-  forEach(fn, type ='entries'){
-    let arr = this[type]
-    for (let index = 0; index < arr.length; index++) {
-      const e = arr[index];
-      if (fn(e[0], e[1], i) === false) return false;
+  generate(fn){
+
+    eachObj(this.__rowValue, (key, value, i)=>{
+      this.keys.push(key);
+      this.values.push(value);
+      this.arr.push([key, value]);
+      fn && fn(key, value, i);
+    })
+
+    this.__hasGenerate = true;
+    return this;
+  }
+
+  forEach(fn){
+    let result = true;
+    if (this.__hasGenerate){
+        retult = eachArr(this.entries, (e,i)=>{
+        return fn(e[0], e[1], i)
+      })
+    }else{
+      let hasFalse = false;
+      this.generate((key, elt, i)=>{
+        if (hasFalse) result = false;
+        hasFalse = fn(key, elt,i);
+      })
     }
-    return true
 
+    return result
   }
 
-  reduce(fn, init, type = 'entries'){
-    this[type].reduce((accu,e,i)=>{
-      return fn(accu, e[0], e[1], i)
-    },init)
+  reduce(fn, init){
+
+    if (!this.__hasGenerate){
+      let value = init;
+      this.generate((key, val,i)=>{
+        value = fn(key, val, i)
+      });
+      return value
+    }else{
+      return this.entries.reduce((accu, e, i) => {
+        return fn(accu, e[0], e[1], i)
+      }, init)
+    }
+    
   }
+}
+
+
+function eachObj(obj,fn){
+  let i = 0;
+  for(let key in obj ){
+    let rut = fn(key, obj[key], i);
+    if(rut===false) return false;
+    i++;
+  }
+  return true;
+}
+
+function eachArr(arr, fn){
+  for (let index = 0; index < arr.length; index++) {
+    const e = arr[index];
+    if (fn(e, i) === false) return false;
+  }
+  return true
 }
