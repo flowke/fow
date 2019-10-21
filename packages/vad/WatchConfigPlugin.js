afd103329cedb2f87678ab37768c56d675b3c00a
春田石雨 <flowke@163.com>
2019-10-21 16:29:53 +0800

    upd

--- packages/vad/WatchConfigPlugin.js
+++ packages/vad/WatchConfigPlugin.js
@@ -1,16 +1,21 @@
-
+require('colors');
 
 module.exports = class WatchConfigPlugin{
- constructor(){} 
+  constructor(){} 
 
- run(runner){
-   runner.hooks.watch.onCall(add=>{
-     add({
-       type: 'restart',
-       name: 'config',
-       paths: ['config/config.js'],
-       events: 'change',
-     })
-   })
- }
+  run(runner){
+    runner.hooks.watch.onCall(add=>{
+    add({
+      type: 'restart',
+      name: 'config',
+      paths: ['config/config.js'],
+      events: 'change',
+      callback: (ctx)=>{
+        console.log('cause file changed, try to restart the server:'.green.bold);
+        console.log(`  file: ${ctx.path}`.green);
+      }
+    });
+
+    })
+  }
 }
